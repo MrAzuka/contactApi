cfceb51a89226d36730bdfff5b7ebf56bdb6fc5a
Azuka Olisemelie <johntwice8@gmail.com>
2021-06-01 11:24:46 +0100

    Authorization

---
+++ middleware/authentication.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const {SECRET} = process.env
+
+exports.authUser = (req,res,next) => {
+    // Check for an authorization token
+    if(!req.headers.authorization){
+        res.status(401).json({message: "Authorization Header required"})
+    }
+    // Split the header and authenticate the token
+    let splitHeader = req.headers.authorization.split(' ')
+    // Check if bearer is included in the format
+    if(splitHeader[0] !== "Bearer"){
+        res.status(401).json({message: "Expected format 'Bearer <token>'"})
+    }
+    // Authenticate the token with jwt
+    let token = splitHeader[1]
+
+    jwt.verify(token, SECRET, (err, decodedToken) => {
+        if (err){
+            res.status(500).json({err})
+        }
+        if (!decodedToken) {
+            res.status(401).json({message: "Authorization error, Please Login"})
+        }
+    })
+    // Move to the next function
+    next()
+}
--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -2,10 +2,11 @@ const {Router} = require('express')
 const router = Router()
 const Contact = require('../models/Contacts')
 const contactController = require('../controllers/contactController')
+const {authUser} = require('../middleware/authentication')
 
 router.post('/api/contact', contactController.createContact)
-router.get('/api/contact', contactController.getAllContact)
-router.get('/api/contact/:id', contactController.getOneContact)
+router.get('/api/contact', authUser, contactController.getAllContact)
+router.get('/api/contact/:id', authUser, contactController.getOneContact)
 router.put('/api/contact/:id', contactController.updateContact)
 router.delete('/api/contact/:id', contactController.deleteContact)
 
