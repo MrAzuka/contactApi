1874587a2f3b57596d530a376fe47a3aa806e73e
Azuka Olisemelie <johntwice8@gmail.com>
2021-09-24 08:10:12 +0100

    Making new updates

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router =  express.Router()
 
-const {signupUser, loginUser} = require('../controllers/authController')
+const {signupUser, loginUser, homePage} = require('../controllers/authController')
 
 router.post('/signup', signupUser)
 router.post('/login', loginUser)
+router.get('/homepage', homePage)
 
 
 module.exports = router
