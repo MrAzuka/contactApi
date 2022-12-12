19bfd393d829fd5815c6f185aa63e4ef3a6ac301
MrAzuka <johntwice8@gmail.com>
2022-12-12 23:57:38 +0100

    refactored admin seeder

--- src/seeders/admin.js
+++ src/seeders/admin.js
@@ -1,36 +1,29 @@
 const Users = require('../models/Users')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
-const {ADMIN_PASSWORD} = process.env
+const { ADMIN_PASSWORD } = process.env
 
-exports.seedAdmin = () => {
-    // check if an admin exist
-    Users.findOne({role: "admin"}, (err,existingAdmin) => {
-        if (err) throw err
-        if(existingAdmin) return "Admin already exists"
+exports.seedAdmin = async () => {
+    try {
+        const checkForAdmin = await Users.findOne({ role: "admin" })
+        if (checkForAdmin) {
+            return "Admin already exists"
+        }
 
-        // create and seed an admin
-        Users.create({
+        // hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(ADMIN_PASSWORD, salt)
+
+        const createAdmin = await Users.create({
             firstName: "Admin",
             lastName: "User",
             email: "Admin@Admin.com",
-            role: "admin"
-        }, (err, newAdmin) => {
-            if(err) throw err
-            // hash the password
-            bcrypt.genSalt(10,(err, salt) => {
-                if(err) throw err
-                
-                bcrypt.hash(ADMIN_PASSWORD, salt, (err, hashedPassword) => {
-                    if (err) throw err
-                    // save the password to the database
-                    newAdmin.password = hashedPassword
-                    newAdmin.save((err, savedAdmin) => {
-                        if (err) throw err
-                        return "Admin created"
-                })
-            })
+            role: "admin",
+            password: hashedPassword
         })
-    })
-})
+        if (createAdmin) return "Admin Created Successfully"
+
+    } catch (err) {
+        return "Error creating Admin"
+    }
 }
